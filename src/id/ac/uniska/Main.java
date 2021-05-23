f05af3730bc6e268b0d0ac99893bbb03820ea39c
ysuwandy <muhammadyuswandy@gmail.com>
2021-05-23 19:57:49 +0800

    Mengubah printout pada Java

--- src/id/ac/uniska/Main.java
+++ src/id/ac/uniska/Main.java
@@ -3,6 +3,6 @@ package id.ac.uniska;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Halo Selamat Belajar Java");
+        System.out.println("Halo Selamat Belajar Java Dengan Idea");
     }
 }
